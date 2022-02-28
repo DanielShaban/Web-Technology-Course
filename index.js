af9f537106e086f7d91ef92761a774ff2adf83e7
krutoyDaniel <55671854+krutoyDaniel@users.noreply.github.com>
2022-02-28 19:03:29 +0300

    correct node js

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ const express = require('express')
 const app = express()
 
 const PORT = process.env.PORT || 80
+app.use(express.static('./'));
 
 app.get('/', (req, res)=>{
-    // res.sendFile('/index.html')
-    res.end('es')
+    res.sendFile('./index.html',  { root : __dirname})
 })
 
-app.get('/math', (req, res)=>{
-    res.sendFile('/math.xml')
+app.get('/math.xml', (req, res)=>{
+    res.sendFile('./math.xml', { root : __dirname})
 })
 
-app.get('/graphics', (req, res)=>{
-    res.sendFile('/graphics.xml')
+app.get('/graphics.xml', (req, res)=>{
+    res.sendFile('./graphics.xml', { root : __dirname})
 })
 
 app.listen(PORT)
