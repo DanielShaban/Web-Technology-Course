efbc49fa05111a4f7f9671ab658cb16b57cf6261
krutoyDaniel <55671854+krutoyDaniel@users.noreply.github.com>
2022-02-28 22:02:20 +0300

    add author

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+
+
+const CORS = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET,POST,DELETE',
+    'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers'
+  };
 
 const PORT = process.env.PORT || 80
-app.use(express.static('./'));
+app.use(express.static('./'),cors()
+);
 
 app.get('/', (req, res)=>{
     res.sendFile('./index.html',  { root : __dirname})
@@ -15,5 +24,12 @@ app.get('/math.xml', (req, res)=>{
 app.get('/graphics.xml', (req, res)=>{
     res.sendFile('./graphics.xml', { root : __dirname})
 })
+
+app.get('/author', (req, res)=>{
+    res.set('Content-Type', 'text/html');
+    res.send(Buffer.from('<h4 id="author" title="GossJS">Шабан Даниэл</h4>'));
+    
+})
+
 
 app.listen(PORT)
